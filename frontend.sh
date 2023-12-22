e9c0e4e0b5b45e51f189f2df2b4006ba267a503d
Queen Florence <queenflorence@Queens-MBP.lan>
2023-12-22 17:13:51 -0600

    commit any change

--- frontend.sh
+++ frontend.sh
@@ -3,11 +3,15 @@ color="\e[33m"
 
 echo -e "${color} installing nginx \e[0m"
 dnf install nginx -y &>>$log_file
+echo $?
 
 echo -e "${color} copy expense config files \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
+
 echo -e "${color} clean old nginx content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
 echo -e "${color} download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
@@ -15,8 +19,10 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 echo -e "${color} extract downloaded application content\e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
+echo $?
 
 
 echo -e "${color} starting nginx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
+echo $?
