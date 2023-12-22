ce8fbaebfb269a6fd9ebc7bf13dd48a3aa06d53a
Queen Florence <queenflorence@Queens-MBP.lan>
2023-12-22 17:49:46 -0600

    commit any change

--- frontend.sh
+++ frontend.sh
@@ -13,23 +13,43 @@ fi
 
 echo -e "${color} copy expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} clean old nginx content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} extract downloaded application content\e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 
 
 echo -e "${color} starting nginx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
